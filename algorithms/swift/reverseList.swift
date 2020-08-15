1a95734846263313786af56dd62407b1aeb838b2
Neo <neo.caixiaoqing@gmail.com>
2020-08-15 15:26:59 +0800

    补充节点 class

--- algorithms/swift/reverseList.swift
+++ algorithms/swift/reverseList.swift
@@ -49,3 +49,12 @@
 //        }
 //        return ret
     }
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val:Int) {
+        self.val = val
+        self.next = nil
+    }
+}
